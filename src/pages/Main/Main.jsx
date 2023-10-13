e7162d13de35749111ced513c50ce45626c3d024
Sheon Lee <dlelfo7@gmail.com>
2023-10-13 19:19:22 -0400

    Add Comment for main components

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -5,9 +5,11 @@ import { axiosGetZones } from "../../utils/AxiosHelper";
 
 
 export default function Main() {
+    // State List of Zone 01 ~ 20
     const [zoneList, setZoneList] = useState(null);
 
     useEffect(() => {
+        // Async function to set ZoneList. GetZones from utils/AxioHelper.js
         async function setList() {
             const { data } = await axiosGetZones();
             setZoneList(data);
@@ -19,14 +21,17 @@ export default function Main() {
 
     if(!zoneList) { return <div>LOADING</div>}
 
+    // Divde point on ZoneList's length for 2 seperated div for mobile view.
     const halfLength = (zoneList.length%2===0 ? zoneList.length/2 : (zoneList.length-1)/2);
 
     return (
         <section className="main">
+            {/* Where the Mapbox goes in */}
             <div className="main__mapbox">
                 <img className="main__dummy-mapbox" src={fmzDummy} alt=""/>
             </div>
             <div className="main__zone-container">
+                {/* Divde Text Container into 2 for better mobile view */}
                 <div className="main__zone-text-container">
                     {zoneList.filter((zone, i) => i<halfLength).map(zone => {
                         return <span className={`main__zone${(zone.id===1? " main__zone--first": "")}`}>{zone.zone}</span>
