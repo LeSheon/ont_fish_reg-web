3aed98b7d2697bc33d37e8b013d5422768f44800
Sheon Lee <dlelfo7@gmail.com>
2023-10-13 17:31:39 -0400

    Move Logo from Footer to Menu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,7 @@
 import "./header.scss";
 import { slide as Menu } from 'react-burger-menu';
+import gitHub from "../../assets/logos/github-mark.svg";
+import linkedIn from "../../assets/logos/linkedin-logo.png";
 
 
 export default function Header() {
@@ -12,9 +14,16 @@ export default function Header() {
         <header className="header">
             <div className="header__hamburger">
                 <Menu>
-                    <a id="home" className="header__menu-item" href="/">Home</a>
-                    <a id="about" className="header__menu-item" href="/about">About</a>
-                    <a id="contact" className="header__menu-item" href="/contact">Contact</a>
+                    <div className="header__menu-container">
+                        <a id="home" className="header__menu-item" href="/">Home</a>
+                        <a id="about" className="header__menu-item" href="/about">About</a>
+                        <a id="contact" className="header__menu-item" href="/contact">Contact</a>
+                    </div>
+
+                    <div className="header__my-container">
+                        <a className="header__my-link" href="https://github.com/LeSheon"><img className="header__my-logo" src={gitHub} alt="GitHub Logo"/></a>
+                        <a className="header__my-link" href="https://www.linkedin.com/in/donghyeon-lee/"><img className="header__my-logo" src={linkedIn} alt="Linked In Logo"/></a>
+                    </div>
                 </Menu>
             </div>
             <div className="header__title-container">
