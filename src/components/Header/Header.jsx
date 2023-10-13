ee7d08700a6023e384dd1d26f704dfc722b756c1
Sheon Lee <dlelfo7@gmail.com>
2023-10-13 16:48:09 -0400

    Add Main Page Components

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,24 @@
 import "./header.scss";
-import title from "../../assets/logos/fyi_ontario_fmz.png";
+import { slide as Menu } from 'react-burger-menu';
+
 
 export default function Header() {
+
+    function showSettings() {
+
+    }
+
     return (
         <header className="header">
-            {/* <div className="header__mobile-nav-container">
-                <nav className="header__mobile-nav">
-                    <ul></ul>
-                </nav>
-            </div> */}
+            <div className="header__hamburger">
+                <Menu>
+                    <a id="home" className="header__menu-item" href="/">Home</a>
+                    <a id="about" className="header__menu-item" href="/about">About</a>
+                    <a id="contact" className="header__menu-item" href="/contact">Contact</a>
+                </Menu>
+            </div>
             <div className="header__title-container">
-                <img className="header__title-image" src={title} alt="FYI Ontario FMZ" />
+                <h2 className="header__title">FYI Ontario FMZ</h2>
             </div>
         </header>
     );
