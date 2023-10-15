8f572fd6b896c6f01e4d59c9b1761f571611940f
Sheon Lee <dlelfo7@gmail.com>
2023-10-15 11:56:00 -0400

    Add FishDetail

--- src/components/FishDetail/FishDetail.jsx
+++ src/components/FishDetail/FishDetail.jsx
@@ -1,9 +1,22 @@
+import { useState } from "react";
 import "./fishDetail.scss";
 
 export default function FishDetail({fishData}) {
+
+    const [showSlide, setShowSlide] = useState(false);
+
     return (
         <>
-            <li className="fish-detail__list-item">{fishData.species}</li>
+            <li className="fish-detail">
+                <a className="fish-detail__show-click" onClick={() => setShowSlide(!showSlide)}><h4 className="fish-detail__species">{fishData.species}</h4></a>
+                <ul className={`fish-detail__slide-list ${(showSlide ? "fish-detail__slide-list--show" : "")}`}>
+                    <li className="fish-detail__slide-item">{fishData.season}</li>
+                    <li className="fish-detail__slide-item">{fishData.s_limit}</li>
+                    <li className="fish-detail__slide-item">{fishData.s_limit_desc}</li>
+                    <li className="fish-detail__slide-item">{fishData.c_limit}</li>
+                    <li className="fish-detail__slide-item">{fishData.c_limit_desc}</li>
+                </ul>
+            </li>
         </>
     );
 }
